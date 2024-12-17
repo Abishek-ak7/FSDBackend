5d0c3b07dd4b17a17e185ac1b031458528be34c8
Abishek-ak7 <abishekcsecs@gmail.com>
2024-12-17 10:09:44 +0530

    Crossorigin setup

--- src/main/java/form/backend/controller/EmployeeController.java
+++ src/main/java/form/backend/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import form.backend.model.Employee;
 
 @RestController
 @RequestMapping("/api/employees")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000","https://fs-dfrontend.vercel.app"})
 public class EmployeeController {
 
     private final EmployeeRepository employeeRepository;
